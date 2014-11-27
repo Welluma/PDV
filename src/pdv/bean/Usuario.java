d4a8cae651c5adb3fd3ab96da2dc395a08998018
Luma <luma.romagnone@gmail.com>
2014-11-27 16:22:38 -0300

    Pacote Bean e começo do pacote pdv.db.dump

---
+++ src/pdv/bean/Usuario.java
@@ -0,0 +1,5 @@
+package pdv.bean;
+
+public class Usuario {
+
+}
---
+++ src/pdv/db/dump/DumpBanco.java
@@ -0,0 +1,25 @@
+package pdv.db.dump;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class DumpBanco {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Começo");
+
+		EntityManager em = null;
+		EntityManagerFactory emf = null;
+		
+		try {
+			Properties pp = new Properties();
+		//	pp.setProperty("hibernate.", value)
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
